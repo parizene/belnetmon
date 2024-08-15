f76bfab6666971e473d50c373f877748f89a3e95
Tim <parizene@gmail.com>
2024-08-15 15:32:17 +0200

    added plausible analytics

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import PlausibleProvider from "next-plausible";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'BELNETMON',
-}
+  title: "BELNETMON",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <PlausibleProvider domain="belnetmon.com" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
